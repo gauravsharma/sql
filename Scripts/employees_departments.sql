23cbe5a96e20f61908da15756b02f4cf57ee99bc
Gaurav Sharma <gaurav.v.sharma@gmail.com>
2022-05-10 14:05:05 +0530

    added sql files for employees db tables

---
+++ Scripts/employees_departments.sql
@@ -0,0 +1,10 @@
+INSERT INTO employees.departments (dept_no,dept_name) VALUES
+	 ('d009','Customer Service'),
+	 ('d005','Development'),
+	 ('d002','Finance'),
+	 ('d003','Human Resources'),
+	 ('d001','Marketing'),
+	 ('d004','Production'),
+	 ('d006','Quality Management'),
+	 ('d008','Research'),
+	 ('d007','Sales');
---
+++ Scripts/employees_salaries.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c8c78a4243caf475046deef74cd55d0abbf2c1c6667073816a44a6cce544acb0
+size 144854764
